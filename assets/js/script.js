1e35d0a45ec45fd569d09958bebefd765904a1e1
Shirong Foo <dee.sr.foo@gmail.com>
2017-03-23 16:05:42 +0800

    modified script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,130 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   // put code inside here
+//   var firstNumInput = document.getElementById('firstNum')
+//   var secondNumInput = document.getElementById('secondNum')
+//   var operatorInput = document.getElementById('operator')
+//   var button = document.getElementById('button')
+//   var resultsContainer = document.getElementById('resultsContainer')
+//
+//   button.addEventListener('click', function () {
+//     button.style.top = '500px'
+//     var firstNum = firstNumInput.value
+//     console.log(firstNum)
+//     var secondNum = secondNumInput.value
+//     var operator = operatorInput.value
+//
+//     // if(firstNum.match(/[0-9]+/) && secondNum.match(/[0-9]+/) && (operator === '+' || operator === '-' || operator === '*' || operator === '/')){
+//     if (firstNum.match(/[0-9]+/) && secondNum.match(/[0-9]+/) && operator.match(/[-+\/*]/)) {
+//       resultsContainer.textContent = eval(firstNum + operator + secondNum)
+//     } else {
+//       alert('not valid input')
+//     }
+//   }) // end of addEventListener
+//
+// }) //end of document.addEventListener
+
+// console.log('it worked')
+//
+// var listItems = document.getElementsByClassName('list-item')
+//
+// console.log(listItems)
+//
+// for (var i = 0; i < listItems.length; i++) {
+//   listItems[i].textContent = 'something'
+// }
+//
+// function list(names){
+//   var finalStr = names.reduce(function(accu, val, index){ //remember to set the return of the annonymous function to a variable!!!
+//   if(index === names.length-1){ //check if we're in the last index
+//   accu += ' & ' + val.name
+//   } else if(index === names.length-2){ //check if we're at right before the last index
+//   accu+= val.name}
+//   else {
+//   accu+= val.name + ', '
+//   }
+//   return accu
+//   }, '') //set the first accu as a string because in this case, the first accu is an object
+//   return finalStr
+// }
+//
+//
+//
+// function printAllNames(){
+//   alert('printAllNames')
+//   var allNames = document.querySelectorAll('.list-item')
+//   list(allNames)
+// }
+//
+// var button = document.querySelector('button')
+// button.addEventListener('click', printAllNames)
+
+$(document).ready(function () {
+  restart()
+  isGameOver()
+  var $container = $('.container')
+  var $boxArray = $container.find('.box')
+
+  $boxArray.on('click', function (e) {
+    var box = (e.target)
+    playTurn($boxArray.index(e.target)) // index for playturn is the indexOf the box array
+    var symbolNum = grid[($boxArray.index(e.target))]
+    if (symbolNum === 1) {
+      box.textContent = 'X'
+    } else {
+      box.textContent = 'O'
+    }
+    // box.textContent = grid[$boxArray.index(e.target)]
+    // var resultOfWhoWon = whoWon()
+    // if (resultOfWhoWon != 0) {
+    //   console.log(resultOfWhoWon)
+    //   restart()
+    // }
+    console.log(whoWon())
+    console.log(isGameOver())
+    if(whoWon() === 1 || whoWon() === 2 || whoWon() === 3){
+
+          location.reload()
+    }
+  })
+
+  var grid = [null, null, null, null, null, null, null, null, null]
+  var player = 1
+  function playTurn (index) {
+    if (grid[index] || isGameOver()) {
+      return false
+    } else {
+      grid[index] = player
+      if (player === 1) player = 2
+      else player = 1
+      return true
+    }
+  }
+  //
+  function isGameOver () {
+    if (whoWon()) {
+      console.log('Game is Over')
+      return true
+    }
+    return false
+  }
+  //
+  function whoWon () {
+    if (grid[0] && grid[0] === grid[1] && grid[0] === grid[2]) return grid[0]
+    if (grid[3] && grid[3] === grid[4] && grid[3] === grid[5]) return grid[3]
+    if (grid[6] && grid[6] === grid[7] && grid[6] === grid[8]) return grid[6]
+    if (grid[0] && grid[0] === grid[3] && grid[0] === grid[6]) return grid[0]
+    if (grid[1] && grid[1] === grid[4] && grid[1] === grid[7]) return grid[1]
+    if (grid[2] && grid[2] === grid[5] && grid[2] === grid[8]) return grid[2]
+    if (grid[0] && grid[0] === grid[4] && grid[0] === grid[8]) return grid[0]
+    if (grid[2] && grid[2] === grid[4] && grid[2] === grid[6]) return grid[2]
+    if (grid[0] && grid[1] && grid[2] && grid[3] && grid[4] &&
+      grid[5] && grid[6] && grid[7] && grid[8]) return 3
+    return 0
+  }
+  //
+  function restart () {
+    grid = [null, null, null, null, null, null, null, null, null]
+    player = 1
+
+  }
+}) // end of document.ready function
--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,91 +1,125 @@
-function restart () {
-  grid = []
-}
+// function restart () {
+//   grid = []
+// }
+// //
+// function isGameOver () {
+//   if (grid.length === 0) return false
+//   // if (grid[0] === 0 && grid[2] === 1 && grid[4] === 2) {
+//   //   return true
+//   if (!whoWon) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 //
-function isGameOver () {
-  if (grid.length === 0) return false
-  // if (grid[0] === 0 && grid[2] === 1 && grid[4] === 2) {
-  //   return true
-  if(!whoWon){
-    return true
-  } else{
+// // simulateGame('P1 Top Row Victory', [0, 3, 1, 4, 2], playerOne)
+// // simulateGame('P2 Top Row Victory', [8, 0, 3, 1, 4, 2], playerTwo)
+// // simulateGame('P1 Middle Row Victory', [3, 0, 4, 1, 5], playerOne)
+// // simulateGame('P2 Middle Row Victory', [8, 3, 0, 4, 1, 5], playerTwo)
+// // simulateGame('P1 Bottom Row Victory', [6, 0, 7, 4, 8], playerOne)
+// // simulateGame('P2 Bottom Row Victory', [2, 6, 0, 7, 4, 8], playerTwo)
+// // simulateGame('P1 Left Col Victory', [0, 4, 3, 5, 6], playerOne)
+// // simulateGame('P2 Left Col Victory', [1, 0, 4, 3, 5, 6], playerTwo)
+// // simulateGame('P1 Middle Col Victory', [1, 2, 4, 3, 7], playerOne)
+// // simulateGame('P2 Middle Col Victory', [0, 1, 2, 4, 3, 7], playerTwo)
+// // simulateGame('P1 Right Col Victory', [2, 1, 5, 3, 8], playerOne)
+// // simulateGame('P2 Right Col Victory', [0, 2, 1, 5, 3, 8], playerTwo)
+// // simulateGame('P1 TL to BR Victory', [0, 2, 4, 5, 8], playerOne)
+// // simulateGame('P1 TL to BR Victory', [1, 0, 2, 4, 5, 8], playerTwo)
+// // simulateGame('P2 TR to BL Victory', [2, 1, 4, 3, 6], playerOne)
+// // simulateGame('P2 TR to BL Victory', [0, 2, 1, 4, 3, 6], playerTwo)
+// // simulateGame('Draw Game', [0, 1, 2, 3, 5, 4, 6, 8, 7], playerDraw)
+//
+// function whoWon () {
+//   if (grid.length === 0) { return 0 }
+//
+//   if (grid[0] === 0 && grid[2] === 1 && grid[4] === 2 ||
+//       grid[0] === 3 && grid[2] === 4 && grid[4] === 5 ||
+//       grid[0] === 6 && grid[2] === 7 && grid[4] === 8 ||
+//       grid[0] === 0 && grid[2] === 3 && grid[4] === 6 ||
+//       grid[0] === 1 && grid[2] === 4 && grid[4] === 7 ||
+//       grid[0] === 2 && grid[2] === 5 && grid[4] === 8 ||
+//       grid[0] === 0 && grid[2] === 4 && grid[4] === 8 ||
+//       grid[0] === 2 && grid[2] === 4 && grid[4] === 6
+//     ) {
+//     return currentPlayer
+//   } else if (
+//       grid[1] === 0 && grid[3] === 1 && grid[5] === 2 ||
+//       grid[1] === 3 && grid[3] === 4 && grid[5] === 5 ||
+//       grid[1] === 6 && grid[3] === 7 && grid[5] === 8 ||
+//       grid[1] === 0 && grid[3] === 3 && grid[5] === 6 ||
+//       grid[1] === 1 && grid[3] === 4 && grid[5] === 7 ||
+//       grid[1] === 2 && grid[3] === 5 && grid[5] === 8 ||
+//       grid[1] === 0 && grid[3] === 4 && grid[5] === 8 ||
+//       grid[1] === 2 && grid[3] === 4 && grid[5] === 6) {
+//     return currentPlayer
+//   } else if (grid.length === 9) {
+//     return 3
+//   } else {
+//     return false
+//   }
+// }
+//
+// function playTurn (index) { // index indicates where the current player's token should be played
+//   // return Boolean value
+//   if (currentPlayer === 1) {
+//     currentPlayer = currentPlayer + 1
+//   } else {
+//     currentPlayer = currentPlayer - 1
+//   }
+//   var varForIsGameOver = isGameOver()
+//   if (!isGameOver) {
+//     return false
+//   } else if (grid.includes(index)) {
+//     return false
+//   } else {
+//     if (varForIsGameOver === false) {
+//       grid.push(index)
+//       console.log(grid)
+//       return true
+//     }
+//   }
+// }
+//
+// var grid = []
+// var currentPlayer = 2
+
+
+
+var grid = [null, null, null, null, null, null, null, null, null]
+var player = 1
+function playTurn (index) {
+  if (grid[index] || isGameOver()) {
     return false
+  } else {
+    grid[index] = player
+    if (player === 1) player = 2
+    else player = 1
+    return true
   }
 }
 
-
-//simulateGame('P1 Top Row Victory', [0, 3, 1, 4, 2], playerOne)
-// simulateGame('P2 Top Row Victory', [8, 0, 3, 1, 4, 2], playerTwo)
-// simulateGame('P1 Middle Row Victory', [3, 0, 4, 1, 5], playerOne)
-// simulateGame('P2 Middle Row Victory', [8, 3, 0, 4, 1, 5], playerTwo)
-// simulateGame('P1 Bottom Row Victory', [6, 0, 7, 4, 8], playerOne)
-// simulateGame('P2 Bottom Row Victory', [2, 6, 0, 7, 4, 8], playerTwo)
-// simulateGame('P1 Left Col Victory', [0, 4, 3, 5, 6], playerOne)
-// simulateGame('P2 Left Col Victory', [1, 0, 4, 3, 5, 6], playerTwo)
-// simulateGame('P1 Middle Col Victory', [1, 2, 4, 3, 7], playerOne)
-// simulateGame('P2 Middle Col Victory', [0, 1, 2, 4, 3, 7], playerTwo)
-// simulateGame('P1 Right Col Victory', [2, 1, 5, 3, 8], playerOne)
-// simulateGame('P2 Right Col Victory', [0, 2, 1, 5, 3, 8], playerTwo)
-// simulateGame('P1 TL to BR Victory', [0, 2, 4, 5, 8], playerOne)
-// simulateGame('P1 TL to BR Victory', [1, 0, 2, 4, 5, 8], playerTwo)
-// simulateGame('P2 TR to BL Victory', [2, 1, 4, 3, 6], playerOne)
-// simulateGame('P2 TR to BL Victory', [0, 2, 1, 4, 3, 6], playerTwo)
-// simulateGame('Draw Game', [0, 1, 2, 3, 5, 4, 6, 8, 7], playerDraw)
-
-
-
+function isGameOver () {
+  if (whoWon()) return true
+  return false
+}
 
 function whoWon () {
-  if (grid.length === 0) { return 0 }
-
-
-  if (grid[0] === 0 && grid[2] === 1 && grid[4] === 2 ||
-      grid[0] === 3 && grid[2] === 4 && grid[4] === 5 ||
-      grid[0] === 6 && grid[2] === 7 && grid[4] === 8 ||
-      grid[0] === 0 && grid[2] === 3 && grid[4] === 6 ||
-      grid[0] === 1 && grid[2] === 4 && grid[4] === 7 ||
-      grid[0] === 2 && grid[2] === 5 && grid[4] === 8 ||
-      grid[0] === 0 && grid[2] === 4 && grid[4] === 8 ||
-      grid[0] === 2 && grid[2] === 4 && grid[4] === 6
-    ) {
-    return currentPlayer
-  } else if(
-      grid[1] === 0 && grid[3] === 1 && grid[5] === 2 ||
-      grid[1] === 3 && grid[3] === 4 && grid[5] === 5 ||
-      grid[1] === 6 && grid[3] === 7 && grid[5] === 8 ||
-      grid[1] === 0 && grid[3] === 3 && grid[5] === 6 ||
-      grid[1] === 1 && grid[3] === 4 && grid[5] === 7 ||
-      grid[1] === 2 && grid[3] === 5 && grid[5] === 8 ||
-      grid[1] === 0 && grid[3] === 4 && grid[5] === 8 ||
-      grid[1] === 2 && grid[3] === 4 && grid[5] === 6){
-    return currentPlayer
-  } else if(grid.length === 9) {
-    return 3
-  } else {
-    return false
-  }
+  if (grid[0] && grid[0] === grid[1] && grid[0] === grid[2]) return grid[0]
+  if (grid[3] && grid[3] === grid[4] && grid[3] === grid[5]) return grid[3]
+  if (grid[6] && grid[6] === grid[7] && grid[6] === grid[8]) return grid[6]
+  if (grid[0] && grid[0] === grid[3] && grid[0] === grid[6]) return grid[0]
+  if (grid[1] && grid[1] === grid[4] && grid[1] === grid[7]) return grid[1]
+  if (grid[2] && grid[2] === grid[5] && grid[2] === grid[8]) return grid[2]
+  if (grid[0] && grid[0] === grid[4] && grid[0] === grid[8]) return grid[0]
+  if (grid[2] && grid[2] === grid[4] && grid[2] === grid[6]) return grid[2]
+  if (grid[0] && grid[1] && grid[2] && grid[3] && grid[4] &&
+    grid[5] && grid[6] && grid[7] && grid[8]) return 3
+  return 0
 }
 
-function playTurn (index) { // index indicates where the current player's token should be played
-  // return Boolean value
-  if(currentPlayer === 1 ){
-    currentPlayer = currentPlayer + 1
-  } else {
-    currentPlayer = currentPlayer - 1
-  }
-  var varForIsGameOver = isGameOver()
-  if (!isGameOver) {
-    return false
-  } else if (grid.includes(index)) {
-    return false
-  } else {
-      if(varForIsGameOver === false){
-      grid.push(index)
-      console.log(grid)
-      return true
-      }
-    }
-  }
-
-var grid = []
-var currentPlayer = 2
+function restart () {
+  grid = [null, null, null, null, null, null, null, null, null]
+  player = 1
+}
